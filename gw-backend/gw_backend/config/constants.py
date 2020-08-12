aa12d384e0e65a5814488bc3d747603d3d2546e7
DerAndre <alittysw@gmail.com>
2020-08-12 16:04:18 +0200

    Removed hard-coded credentials and constants Implemented sha3-512 and json file based credential check on login

--- gw-backend/gw_backend/config/constants.py
+++ gw-backend/gw_backend/config/constants.py
@@ -4,9 +4,7 @@
 # @Email: alittysw@gmail.com
 # @Create At: 2020-03-21 13:44:40
 # @Last Modified By: Andre Litty
-# @Last Modified At: 2020-04-05 20:59:01
+# @Last Modified At: 2020-08-12 16:02:34
 # @Description: Application wide configurations / constants.
 
 API_PATH = '/api/v1/'
-TEST_USER = 'admin'
-TEST_PASSWORD = 'admin!'
