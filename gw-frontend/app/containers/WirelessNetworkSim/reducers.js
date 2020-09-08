5ad1b81250ec84522dae3d580e0a2fa9e1ffb129
DerAndre <alittysw@gmail.com>
2020-09-08 12:54:14 +0100

    Added empty initial state values for GsmReducer

--- gw-frontend/app/containers/WirelessNetworkSim/reducers.js
+++ gw-frontend/app/containers/WirelessNetworkSim/reducers.js
@@ -4,13 +4,23 @@
  * @Email: alittysw@gmail.com
  * @Create At: 2020-08-17 13:15:14
  * @Last Modified By: Andre Litty
- * @Last Modified At: 2020-08-17 13:19:21
+ * @Last Modified At: 2020-09-08 12:49:38
  * @Description: This is description.
  */
 
 import { SUCCESS_SET_MODEM, SUCCESS_GET_MODEM_INFO } from './constants';
 
-const initialState = {};
+const initialState = {
+  GPP: {
+    operatorname: '',
+    imei: '',
+  },
+  Status: {
+    state: '',
+    signalquality: '',
+    powerstate: '',
+  },
+};
 
 function GsmReducer(state = initialState, action) {
   switch (action.key) {
